fb6d1fe56720393a2789c71780b70eedddf4373d
marcios2sanguejovem-del <marcios2sanguejovem@gmail.com>
2026-05-07 22:12:19 -0300

    Add files via upload

---
+++ lista 1 exe1/lista 1 exe1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_1_exe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int a;
+
+            Console.WriteLine("digite um valor para uns dos lados: ");
+            l = int.Parse(Console.ReadLine());
+
+            a = l * l;
+
+            Console.WriteLine("A área do quadrado que tem lado de {0} é {1}", l, a);
+
+
+        }
+    }
+}
